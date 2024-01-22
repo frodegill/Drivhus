bc37d873db51d667c1049a5ba51b7b9cdd1fa570
Frode Roxrud Gill <frodegill@gmail.com>
2024-01-22 11:15:07 +0100

    Turn growlight on/off based on sunrise/sunset

--- drivhus/growlight.cpp
+++ drivhus/growlight.cpp
@@ -10,14 +10,42 @@
 
 
 Drivhus::Growlight::Growlight(uint8_t pin)
-: m_pin(pin)
+: m_pin(pin),
+  m_is_activated(false)
 {
 }
 
 bool Drivhus::Growlight::init() {
   pinMode(m_pin, OUTPUT);
+  digitalWrite(m_pin, m_is_activated ? HIGH : LOW);
   return true;
 }
 
 void Drivhus::Growlight::loop() {
+  const struct tm* local_tm = Drivhus::getNTP()->getLocalTm();
+  if (local_tm != nullptr) {
+    bool within_growlight_period;
+    if (m_sunrise == m_sunset) {
+      within_growlight_period = false;
+    } else {
+      float day_pos = local_tm->tm_hour*60 + local_tm->tm_min + local_tm->tm_sec/60.0;
+      bool within_growlight_period = (day_pos>=m_sunrise && day_pos<=m_sunset);
+      if (m_sunrise > m_sunset) {
+        within_growlight_period = !within_growlight_period;
+      }
+    }
+
+    if (within_growlight_period != m_is_activated) {
+      digitalWrite(m_pin, within_growlight_period ? HIGH : LOW);
+      m_is_activated = within_growlight_period;
+    }
+  }
+}
+
+void Drivhus::Growlight::onSunriseChanged(float value) {
+  m_sunrise = value;
+}
+
+void Drivhus::Growlight::onSunsetChanged(float value) {
+  m_sunset = value;
 }
--- drivhus/growlight.h
+++ drivhus/growlight.h
@@ -3,17 +3,28 @@
 
 #include <stdint.h>
 
+#include "settings.h"
+
+
 namespace Drivhus {
 
-class Growlight
+class Growlight : public OnChangeListener
 {
 public:
   Growlight(uint8_t pin);
   [[nodiscard]] bool init();
   void loop();
 
+protected:
+  virtual void onSunriseChanged(float value) override;
+  virtual void onSunsetChanged(float value) override;
+
 private:
   uint8_t m_pin;
+
+  bool m_is_activated;
+  float m_sunrise;
+  float m_sunset;
 };
 
 } //namespace
